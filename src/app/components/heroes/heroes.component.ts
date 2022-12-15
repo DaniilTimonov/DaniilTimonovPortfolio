4adb8fcb94df53775b0d06c9c8efa8909e9fabdf
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-12-15 10:56:09 +0300

    dd

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -18,6 +18,10 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  onClose() {
+    delete this.selectedHero ;
+  }
+
 
 
   ngOnInit(): void {
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'https://cdnb.artstation.com/p/assets/covers/images/013/228/559/large/william-bobant-william-bobant-turn02-00000.jpg?1538650046' },
-  { id: 13, name: 'https://i.pinimg.com/originals/e9/c4/a9/e9c4a90be4303cfdf18fd5e61045b4c0.jpg' },
-  { id: 14, name: 'https://img2.reactor.cc/pics/post/Blizzard-фэндомы-Ghost-%28SC%29-Starcraft-Units-5826702.jpeg' },
-  { id: 15, name: 'https://i.playground.ru/i/pix/912965/image.jpg' },
-  { id: 16, name: 'https://img2.joyreactor.cc/pics/post/full/Blizzard-фэндомы-Swarm-Host-Zerg-6498456.jpeg' },
-  { id: 17, name: 'https://cdn.paperhi.com/1366x768/20130317/zerg%20terran%20starcraft%20ii%201366x768%20wallpaper_www.paperhi.com_3.jpg' },
-  { id: 18, name: 'https://i.artfile.ru/1920x1200_1032223_[www.ArtFile.ru].jpg' },
-  { id: 19, name: 'https://i.pinimg.com/originals/87/80/96/878096455b3cc907760a7bcd12fa7c7f.jpg' },
-  { id: 20, name: 'http://i85.fastpic.org/big/2016/1208/4f/c425c5066f858543fe3e58b18a20bf4f.jpg' }
+  { id: 1, imgs: 'https://cdnb.artstation.com/p/assets/covers/images/013/228/559/large/william-bobant-william-bobant-turn02-00000.jpg?1538650046' },
+  { id: 2, imgs: 'https://i.pinimg.com/originals/e9/c4/a9/e9c4a90be4303cfdf18fd5e61045b4c0.jpg' },
+  { id: 3, imgs: 'https://img2.reactor.cc/pics/post/Blizzard-фэндомы-Ghost-%28SC%29-Starcraft-Units-5826702.jpeg' },
+  { id: 4, imgs: 'https://i.playground.ru/i/pix/912965/image.jpg' },
+  { id: 5, imgs: 'https://img2.joyreactor.cc/pics/post/full/Blizzard-фэндомы-Swarm-Host-Zerg-6498456.jpeg' },
+  { id: 6, imgs: 'https://cdn.paperhi.com/1366x768/20130317/zerg%20terran%20starcraft%20ii%201366x768%20wallpaper_www.paperhi.com_3.jpg' },
+  { id: 7, imgs: 'https://i.artfile.ru/1920x1200_1032223_[www.ArtFile.ru].jpg' },
+  { id: 8, imgs: 'https://i.pinimg.com/originals/87/80/96/878096455b3cc907760a7bcd12fa7c7f.jpg' },
+  { id: 9, imgs: 'http://i85.fastpic.org/big/2016/1208/4f/c425c5066f858543fe3e58b18a20bf4f.jpg' }
 ];
