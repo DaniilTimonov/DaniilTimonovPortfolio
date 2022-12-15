31a74ce291a66d5c25e6608462c50f0fce65211d
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-12-16 00:35:27 +0300

    implement zerg album

--- src/app/components/zerg-units/zerg-units.component.ts
+++ src/app/components/zerg-units/zerg-units.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
-import { HEROES } from 'src/app/terran-heroes';
+import { HEROES } from 'src/app/zerg-heroes';
 import { Input } from '@angular/core';
 
 
 @Component({
-  selector: 'terran-units',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'zerg-units',
+  templateUrl: './zerg-units.component.html',
+  styleUrls: ['./zerg-units.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class ZergUnitsComponent implements OnInit {
   heroes = HEROES;
 
   constructor() { }
