18d9d6680d4e408fd9d8226ec5a992e5ed97ec73
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-12-21 17:10:09 +0300

    coloring

--- src/app/components/navigationburger/navigationburger.component.ts
+++ src/app/components/navigationburger/navigationburger.component.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigationburger.component.css']
 })
 export class NavigationburgerComponent implements OnInit {
+NavigationData = [
+  {
+    id: 0,
+    title:'Главная',
+    link:'/main'
+  },
+  {
+    id: 1,
+    title:'Рисунки',
+    link:'/traditionalart'
+  },
+  {
+    id: 2,
+    title:'Компьютерная графика',
+    link:'/digitalgraphic'
+  },
+  {
+    id: 3,
+    title:'Рисунки на одежде',
+    link:'/clothdrawing'
+  },
+  {
+    id: 4,
+    title:'Фриланс работы',
+    link:'/freelanceart'
+  },
+  {
+    id: 5,
+    title:'Контакты',
+    link:'/contacts'
+  }
+
+
+]
+
 
   constructor() { }
 
