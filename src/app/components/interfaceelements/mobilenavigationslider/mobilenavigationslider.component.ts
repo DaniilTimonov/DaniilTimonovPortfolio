0939e6224234838e85e9526fa133274b17a24682
DaniilTimonov <daniil.timonov1988@mail.ru>
2023-01-03 22:02:23 +0300

    доработка мобильной версии

--- src/app/components/interfaceelements/mobilenavigationslider/mobilenavigationslider.component.ts
+++ src/app/components/interfaceelements/mobilenavigationslider/mobilenavigationslider.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mobilenavigationslider.component.css']
 })
 export class MobilenavigationsliderComponent implements OnInit {
+  NavigationData = [
+    {
+      id: 0,
+      title:'Главная',
+      link:'/main'
+    },
+    {
+      id: 1,
+      title:'Рисунки',
+      link:'/traditionalart'
+    },
+    {
+      id: 2,
+      title:'Компьютерная графика',
+      link:'/digitalgraphic'
+    },
+    {
+      id: 3,
+      title:'Рисунки на одежде',
+      link:'/clothdrawing'
+    },
+    {
+      id: 4,
+      title:'Фриланс работы',
+      link:'/freelanceart'
+    },
+    {
+      id: 5,
+      title:'Контакты',
+      link:'/contacts'
+    }
+  
+  
+  ]
+  
+  
+  
+  
   opened = false;
 
   toggleSidebar() {
