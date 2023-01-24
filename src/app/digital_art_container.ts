b822a28d2dc7ab805cf5042c8639994ef4e8fe5d
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2023-01-24 17:38:24 +0300

    доработка верстка

--- src/app/digital_art_container.ts
+++ src/app/digital_art_container.ts
@@ -2,19 +2,19 @@ import { Picture } from './picture';
 
 export const PICTURES: Picture[] = [
   { id: 1, imgs: '/assets/images/digitaldraw/digitaldraw01.jpg',
-name:'open full', classtyle: 'width:43.6%' },
+name:'open full', classtyle: '' },
   { id: 2, imgs: '/assets/images/digitaldraw/digitaldraw02.jpg',
   name:'open full' , classtyle: '' },
   { id: 3, imgs: '/assets/images/digitaldraw/digitaldraw03.jpg' ,
   name:'open full' , classtyle: '' },
   { id: 4, imgs: '/assets/images/digitaldraw/digitaldraw04.jpg' ,
-  name:'open full' , classtyle: 'width: 47.5%' },
+  name:'open full' , classtyle: '' },
   { id: 5, imgs: '/assets/images/digitaldraw/digitaldraw05.jpg' ,
-  name:'open full' , classtyle: 'width:47.5%' },
+  name:'open full' , classtyle: '' },
   { id: 6, imgs: '/assets/images/digitaldraw/digitaldraw06.jpg' ,
-  name:'open full' , classtyle: 'height:400px; width:300px'},
+  name:'open full' , classtyle: ''},
   { id: 7, imgs: '/assets/images/digitaldraw/digitaldraw07.jpg' ,
-  name:'open full', classtyle: 'width:66.5%' },
+  name:'open full', classtyle: '' },
   { id: 8, imgs: '/assets/images/digitaldraw/digitaldraw08.jpg' ,
   name:'open full' , classtyle: '' }
 
--- src/app/freelance_art_container.ts
+++ src/app/freelance_art_container.ts
@@ -2,24 +2,24 @@ import { Picture } from './picture';
 
 export const PICTURES: Picture[] = [
   { id: 1, imgs: 'assets/images/freelance/freelance01.jpg' , 
-name:'open full', classtyle: 'width:89%; '},
+name:'open full', classtyle: ''},
   { id: 2, imgs: 'assets/images/freelance/freelance02.jpg',
-  name:'open full' , classtyle: 'width:89%;' },
+  name:'open full' , classtyle: '' },
   { id: 3, imgs: 'assets/images/freelance/freelance03.jpg' ,
-  name:'open full' , classtyle: 'width:50%' },
+  name:'open full' , classtyle: '' },
   { id: 4, imgs: 'assets/images/freelance/freelance04.jpg' ,
-  name:'open full', classtyle: 'width:89%' },
+  name:'open full', classtyle: '' },
   { id: 5, imgs: 'assets/images/freelance/freelance05.jpg' ,
-  name:'open full' , classtyle: 'width:40%' },
+  name:'open full' , classtyle: '' },
   { id: 6, imgs: 'assets/images/freelance/freelance06.jpg' ,
-  name:'open full' , classtyle: 'width:40%'},
+  name:'open full' , classtyle: ''},
   { id: 7, imgs: 'assets/images/freelance/freelance07.jpg' ,
-  name:'open full' , classtyle: 'width:40%' },
+  name:'open full' , classtyle: '' },
   { id: 8, imgs: 'assets/images/freelance/freelance08.jpg' ,
-  name:'open full', classtyle: 'width:40%' },
+  name:'open full', classtyle: '' },
   { id: 9, imgs: 'assets/images/freelance/freelance09.jpg' ,
-  name:'open full' , classtyle: 'width:40%' },
+  name:'open full' , classtyle: '' },
   { id: 10, imgs: 'assets/images/freelance/freelance10.jpg' ,
-  name:'open full' , classtyle: 'width:50%' }
+  name:'open full' , classtyle: '' }
 
 ];
