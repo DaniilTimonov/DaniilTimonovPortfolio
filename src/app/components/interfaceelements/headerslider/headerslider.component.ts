9084898956d4c350ccab055558b04b61ae485ec1
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2023-02-03 01:31:02 +0300

    fixed errors

--- src/app/components/interfaceelements/headerslider/headerslider.component.ts
+++ src/app/components/interfaceelements/headerslider/headerslider.component.ts
@@ -11,42 +11,42 @@ export class HeadersliderComponent implements OnInit {
 	images: any[] = [
 		{
 			previewImageSrc:
-'assets/images/favouritesslider/classicart6.jpg',
+'',
 
 			alt: '',
 			title: ''
 		},
 		{
 			previewImageSrc:
-'assets/images/favouritesslider/clothdraw02.jpg',
+'',
 
 			alt: '',
 			title: ''
 		},
 		{
 			previewImageSrc:
-				'assets/images/favouritesslider/digitaldraw02.jpg',
+				'',
 	
 			alt: '',
 			title: ''
 		},
 		{
 			previewImageSrc:
-			'assets/images/favouritesslider/digitaldraw07.jpg',
+			'',
 		
 			alt: '',
 			title: ''
 		},
 		{
 			previewImageSrc:
-			'assets/images/favouritesslider/freelance03.jpg',
+			'',
 		
 			alt: '',
 			title: ''
 		},
 		{
 			previewImageSrc:
-			'assets/images/favouritesslider/freelance08.jpg',
+			'',
 		
 			alt: '',
 			title: ''
