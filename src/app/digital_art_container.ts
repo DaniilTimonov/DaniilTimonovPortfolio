a165a7610d66e6efb3f449fcd28447d172ed12d1
DaniilTimonov <daniil.timonov1988@mail.ru>
2023-02-04 13:09:26 +0300

    доработка стилей

--- src/app/digital_art_container.ts
+++ src/app/digital_art_container.ts
@@ -2,19 +2,19 @@ import { Picture } from './picture';
 
 export const PICTURES: Picture[] = [
   { id: 1, imgs: '/assets/images/digitaldraw/digitaldraw01.jpg',
-name:'open full', classtyle: '' },
+name:'open full', classtyle: 'width:43%' },
   { id: 2, imgs: '/assets/images/digitaldraw/digitaldraw02.jpg',
   name:'open full' , classtyle: '' },
   { id: 3, imgs: '/assets/images/digitaldraw/digitaldraw03.jpg' ,
   name:'open full' , classtyle: '' },
   { id: 4, imgs: '/assets/images/digitaldraw/digitaldraw04.jpg' ,
-  name:'open full' , classtyle: '' },
+  name:'open full' , classtyle: 'width:47%' },
   { id: 5, imgs: '/assets/images/digitaldraw/digitaldraw05.jpg' ,
-  name:'open full' , classtyle: '' },
+  name:'open full' , classtyle: 'width:47%' },
   { id: 6, imgs: '/assets/images/digitaldraw/digitaldraw06.jpg' ,
-  name:'open full' , classtyle: ''},
+  name:'open full' , classtyle: 'width:40%'},
   { id: 7, imgs: '/assets/images/digitaldraw/digitaldraw07.jpg' ,
-  name:'open full', classtyle: '' },
+  name:'open full', classtyle: 'width:66%' },
   { id: 8, imgs: '/assets/images/digitaldraw/digitaldraw08.jpg' ,
   name:'open full' , classtyle: '' }
 
